7bfb8be145f6b5349096f6ddf2771a802f9835c6
Priyanka Negi <prinegi100@gmail.com>
2018-06-03 20:17:07 +0530

    created dummy route GET

---
+++ index.js
@@ -0,0 +1,19 @@
+const hapi = require('hapi');
+
+const server = hapi.server({
+  port: 4000,
+  host: 'localhost'
+});
+
+const init = async () => {
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      return `<h1>My modern api</h1>`;
+    }
+  });
+  await server.start();
+};
+
+init();
