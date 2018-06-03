08b9937c70486b33ec75c8fabce2719ee98aefbb
Priyanka Negi <prinegi100@gmail.com>
2018-06-03 21:21:02 +0530

    created GET api/v1/paintings & Painting model

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
 
 mongoose.connect('mongodb://localhost/modern-api');
 mongoose.connection.once('open', () => {
@@ -11,13 +12,28 @@ const server = hapi.server({
 });
 
 const init = async () => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-      return `<h1>My modern api</h1>`;
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: function(request, reply) {
+        return `<h1>My modern api</h1>`;
+      }
+    },
+    {
+      method: 'GET',
+      path: 'api/v1/paintings',
+      handler: (req, reply) => {
+        const { name, url, techniques } = req.payload;
+        const painting = new Painting({
+          name,
+          url,
+          techniques
+        });
+        return painting.find();
+      }
     }
-  });
+  ]);
   await server.start();
 };
 
